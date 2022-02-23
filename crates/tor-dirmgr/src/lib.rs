c4dt/arti

dirmgr: add Store trait

crates/tor-dirmgr/src/lib.rs
@@ -69,7 +69,7 @@ mod storage;
 
 use crate::docid::{CacheUsage, ClientRequest, DocQuery};
 use crate::shared_ref::SharedMutArc;
-use crate::storage::sqlite::SqliteStore;
+use crate::storage::DynStore;
 use postage::watch;
 pub use retry::DownloadSchedule;
 use tor_circmgr::CircMgr;
@@ -118,7 +118,8 @@ pub struct DirMgr<R: Runtime> {
     /// Handle to our sqlite cache.
     // TODO(nickm): I'd like to use an rwlock, but that's not feasible, since
     // rusqlite::Connection isn't Sync.
-    store: Mutex<SqliteStore>,
+    // TODO is needed?
+    store: Mutex<DynStore>,
     /// Our latest sufficiently bootstrapped directory, if we have one.
     ///
     /// We use the RwLock so that we can give this out to a bunch of other
@@ -483,7 +484,7 @@ impl<R: Runtime> DirMgr<R> {
     }
 
     /// Return a reference to the store, if it is currently read-write.
-    fn store_if_rw(&self) -> Option<&Mutex<SqliteStore>> {
+    fn store_if_rw(&self) -> Option<&Mutex<DynStore>> {
         let rw = !self
             .store
             .lock()
@@ -504,7 +505,7 @@ impl<R: Runtime> DirMgr<R> {
         circmgr: Option<Arc<CircMgr<R>>>,
         readonly: bool,
     ) -> Result<Self> {
-        let store = Mutex::new(config.open_sqlite_store(readonly)?);
+        let store = Mutex::new(config.open_store(readonly)?);
         let netdir = SharedMutArc::new();
         let events = event::FlagPublisher::new();
 
@@ -783,7 +784,7 @@ trait DirState: Send {
     fn add_from_cache(
         &mut self,
         docs: HashMap<DocId, DocumentText>,
-        storage: Option<&Mutex<SqliteStore>>,
+        storage: Option<&Mutex<DynStore>>,
     ) -> Result<bool>;
 
     /// Add information that we have just downloaded to this state; returns
@@ -803,7 +804,7 @@ trait DirState: Send {
         &mut self,
         text: &str,
         request: &ClientRequest,
-        storage: Option<&Mutex<SqliteStore>>,
+        storage: Option<&Mutex<DynStore>>,
     ) -> Result<bool>;
     /// Return a summary of this state as a [`DirStatus`].
     fn bootstrap_status(&self) -> event::DirStatus;
@@ -884,7 +885,7 @@ mod test {
 
                 store
                     .store_microdescs(
-                        vec![
+                        &[
                             ("Fake micro 1", &d1),
                             ("Fake micro 2", &d2),
                             ("Fake micro 3", &d3),
@@ -895,7 +896,7 @@ mod test {
 
                 #[cfg(feature = "routerdesc")]
                 store
-                    .store_routerdescs(vec![("Fake rd1", now, &d4), ("Fake rd2", now, &d5)])
+                    .store_routerdescs(&[("Fake rd1", now, &d4), ("Fake rd2", now, &d5)])
                     .unwrap();
 
                 store

s7g4/arti-hs-keymgmt

crates/tor-dirmgr/src/lib.rs
@@ -69,7 +69,7 @@ mod storage;
 
 use crate::docid::{CacheUsage, ClientRequest, DocQuery};
 use crate::shared_ref::SharedMutArc;
-use crate::storage::sqlite::SqliteStore;
+use crate::storage::DynStore;
 use postage::watch;
 pub use retry::DownloadSchedule;
 use tor_circmgr::CircMgr;
@@ -119,7 +119,8 @@ pub struct DirMgr<R: Runtime> {
     /// Handle to our sqlite cache.
     // TODO(nickm): I'd like to use an rwlock, but that's not feasible, since
     // rusqlite::Connection isn't Sync.
-    store: Mutex<SqliteStore>,
+    // TODO is needed?
+    store: Mutex<DynStore>,
     /// Our latest sufficiently bootstrapped directory, if we have one.
     ///
     /// We use the RwLock so that we can give this out to a bunch of other
@@ -603,7 +604,7 @@ impl<R: Runtime> DirMgr<R> {
     }
 
     /// Return a reference to the store, if it is currently read-write.
-    fn store_if_rw(&self) -> Option<&Mutex<SqliteStore>> {
+    fn store_if_rw(&self) -> Option<&Mutex<DynStore>> {
         let rw = !self
             .store
             .lock()
@@ -627,7 +628,7 @@ impl<R: Runtime> DirMgr<R> {
         circmgr: Option<Arc<CircMgr<R>>>,
         offline: bool,
     ) -> Result<Self> {
-        let store = Mutex::new(config.open_sqlite_store(offline)?);
+        let store = Mutex::new(config.open_store(offline)?);
         let netdir = SharedMutArc::new();
         let events = event::FlagPublisher::new();
 
@@ -908,7 +909,7 @@ trait DirState: Send {
     fn add_from_cache(
         &mut self,
         docs: HashMap<DocId, DocumentText>,
-        storage: Option<&Mutex<SqliteStore>>,
+        storage: Option<&Mutex<DynStore>>,
     ) -> Result<bool>;
 
     /// Add information that we have just downloaded to this state; returns
@@ -928,7 +929,7 @@ trait DirState: Send {
         &mut self,
         text: &str,
         request: &ClientRequest,
-        storage: Option<&Mutex<SqliteStore>>,
+        storage: Option<&Mutex<DynStore>>,
     ) -> Result<bool>;
     /// Return a summary of this state as a [`DirStatus`].
     fn bootstrap_status(&self) -> event::DirStatus;
@@ -1009,7 +1010,7 @@ mod test {
 
                 store
                     .store_microdescs(
-                        vec![
+                        &[
                             ("Fake micro 1", &d1),
                             ("Fake micro 2", &d2),
                             ("Fake micro 3", &d3),
@@ -1020,7 +1021,7 @@ mod test {
 
                 #[cfg(feature = "routerdesc")]
                 store
-                    .store_routerdescs(vec![("Fake rd1", now, &d4), ("Fake rd2", now, &d5)])
+                    .store_routerdescs(&[("Fake rd1", now, &d4), ("Fake rd2", now, &d5)])
                     .unwrap();
 
                 store